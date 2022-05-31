155621b6c6ddfb7f81c971effc20bca552f4fafe
Fengyu Wang <z5187561@ad.unsw.edu.au>
2022-05-31 22:46:50 +1000

    Added nav bars in the bottom. Next task: make it more intersting to interact with when hovering, such as displaying what it is and zoom in the icon

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <div
+      style={{
+        // position: "fixed",
+        // left: "0",
+        // bottom: "0",
+        // right: "0",
+        // backgroundColor: "blue",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "lightblue",
+          minWidth: "100%",
+          bottom: "0",
+        }}
+      >
+        Copyright by Alan Wang
+      </div>
+    </div>
+  );
+};
+
+export default footer;
---
+++ frontend/src/pages/study/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Study = () => {
+  return <div>This is study</div>;
+};
+
+export default Study;
